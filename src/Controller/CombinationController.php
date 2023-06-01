4964eb1bc41b0cdb5576a55854d0d28549139dca
scarymovie <81684836+scarymovie@users.noreply.github.com>
2023-06-01 17:38:34 +0300

    добавил try catch

--- src/Controller/CombinationController.php
+++ src/Controller/CombinationController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class CombinationController extends AbstractController
 {
@@ -21,22 +24,27 @@ class CombinationController extends AbstractController
 	#[Route('/combination', name: 'combination')]
 	public function index(Request $request): Response
 	{
-		$inputString = $request->get('data');
+		try {
+			$inputString = $request->get('data');
 
-		if (empty($inputString)) {
-			return new Response('Input string is empty', Response::HTTP_BAD_REQUEST);
+			if (empty($inputString)) {
+				throw new \InvalidArgumentException('Input string is empty');
+			}
+
+			$combination = new Combination($inputString);
+
+			$response = [
+				'variants' => $combination->getVariants(),
+				'count' => $combination->getCount(),
+			];
+
+			return new Response(
+				$this->twig->render('combination/index.html.twig', $response),
+				Response::HTTP_OK
+			);
+		} catch (\InvalidArgumentException $e) {
+			return new Response($e->getMessage(), Response::HTTP_BAD_REQUEST);
 		}
-
-		$combination = new Combination($inputString);
-
-		$response = [
-			'variants' => $combination->getVariants(),
-			'count' => $combination->getCount(),
-		];
-
-		return new Response(
-			$this->twig->render('combination/index.html.twig', $response),
-			Response::HTTP_OK
-		);
 	}
+
 }
